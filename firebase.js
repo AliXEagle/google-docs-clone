6ed082efbd3131b59dd1bab38d13a9e60c2a50c9
alixeagle <alicruzaguila@gmail.com>
2021-07-16 09:21:08 -0500

    googleDocsClone ProjectCompleted not deployed fixed env

--- firebase.js
+++ firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-	apiKey: process.env.FIREBASE_API,
+	apiKey: process.env.FIREBASE_API_KEY,
 	authDomain: 'docs-clone-738dc.firebaseapp.com',
 	projectId: 'docs-clone-738dc',
 	storageBucket: 'docs-clone-738dc.appspot.com',
 	messagingSenderId: '102759769916',
-	appId: '1:102759769916:web:db390e8a46feb2cdb42039',
+	appId: process.env.FIREBASE_API_ID,
 };
 
 const app = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
